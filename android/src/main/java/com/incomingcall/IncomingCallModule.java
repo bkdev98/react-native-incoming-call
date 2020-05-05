bc086c20112b7c01edbdd1338b45c2dfa56bbb6a
Quoc Khanh <bkdev98@gmail.com>
2020-05-05 16:55:35 +0700

    fix

--- android/src/main/java/com/incomingcall/IncomingCallModule.java
+++ android/src/main/java/com/incomingcall/IncomingCallModule.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 public class IncomingCallModule extends ReactContextBaseJavaModule {
 
