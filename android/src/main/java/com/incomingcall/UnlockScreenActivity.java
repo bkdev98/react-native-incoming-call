53a76e3dad256cb6ee8f74660cc50dd81666e29e
Quoc Khanh <khanh@quocs.com>
2020-08-20 11:26:54 +0700

    fix background

--- android/src/main/java/com/incomingcall/UnlockScreenActivity.java
+++ android/src/main/java/com/incomingcall/UnlockScreenActivity.java
@@ -126,10 +126,9 @@ public class UnlockScreenActivity extends AppCompatActivity implements UnlockScr
         params.putBoolean("done", true);
         params.putString("uuid", uuid);
 
-        if (IncomingCallModule.reactContext.hasCurrentActivity() && isAppOnForeground(IncomingCallModule.reactContext)) {
-            // App in foreground, send event for app to listen
-            sendEvent("answerCall", params);
-        } else {
+        sendEvent("answerCall", params);
+
+        if (!foregrounded()) {
             // App in background or killed, start app and add launch params
             String packageNames = IncomingCallModule.reactContext.getPackageName();
             Intent launchIntent = IncomingCallModule.reactContext.getPackageManager().getLaunchIntentForPackage(packageNames);
@@ -200,23 +199,10 @@ public class UnlockScreenActivity extends AppCompatActivity implements UnlockScr
                 .emit(eventName, params);
     }
 
-    private boolean isAppOnForeground(ReactApplicationContext context) {
-        /**
-         * We need to check if app is in foreground otherwise the app will crash.
-         * http://stackoverflow.com/questions/8489993/check-android-application-is-in-foreground-or-not
-         **/
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
-        if (appProcesses == null) {
-            return false;
-        }
-        final String packageName = context.getPackageName();
-        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
-            if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
-                    && appProcess.processName.equals(packageName)) {
-                return true;
-            }
-        }
-        return false;
+    @Override
+    public boolean foregrounded() {
+        ActivityManager.RunningAppProcessInfo appProcessInfo = new ActivityManager.RunningAppProcessInfo();
+        ActivityManager.getMyMemoryState(appProcessInfo);
+        return (appProcessInfo.importance == IMPORTANCE_FOREGROUND || appProcessInfo.importance == IMPORTANCE_VISIBLE)
     }
 }
