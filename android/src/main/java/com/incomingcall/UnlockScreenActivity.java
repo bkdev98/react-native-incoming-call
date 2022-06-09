75d232e83b0c76a65cc14aa87b9ea5e1082730f8
Geraint White <geraint.white@bt.com>
2022-06-09 10:03:45 +0100

    Allow customising of styles

--- android/src/main/java/com/incomingcall/UnlockScreenActivity.java
+++ android/src/main/java/com/incomingcall/UnlockScreenActivity.java
@@ -14,6 +14,7 @@ import android.os.Vibrator;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.provider.Settings;
+import android.graphics.Typeface;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -95,6 +96,12 @@ public class UnlockScreenActivity extends AppCompatActivity implements UnlockScr
             if (bundle.containsKey("info")) {
                 String info = bundle.getString("info");
                 tvInfo.setText(info);
+            }
+            if (bundle.containsKey("font")) {
+                String font = bundle.getString("font");
+                Typeface tf = Typeface.createFromAsset(getAssets(), font);
+                tvName.setTypeface(tf);
+                tvInfo.setTypeface(tf);
             }
             if (bundle.containsKey("avatar")) {
                 String avatar = bundle.getString("avatar");
